8d88b0a162252eae130b01eab4ec3dcfc445c3c4
1000zoo <cjswldn99@gmail.com>
2024-04-15 19:09:04 +0900

    feat: 차트 ui

--- project/app/views.py
+++ project/app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 import json
 
+test_data = {"Python": 5, "Java": 4, "C++": 3, "Django": 3, "Spring": 4}
+
 # Create your views here.
 def index(request):
-    test_data = {"Python": 5, "Java": 4, "C++": 3, "Django": 3, "Spring": 4}
     context = {"hotkeywords": test_data}
     return render(request, 'hotkeyword/index.html', context=context)
+
+def chart(request):
+    context = {"hotkeywords": json.dumps(test_data)}
+    return render(request, 'hotkeyword/chart.html', context=context)
