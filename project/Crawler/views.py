6943a8ffeae0b2afb758c04f3963ffbc4adcc2f8
김지원 <kjw44200@naver.com>
2024-04-15 17:22:00 +0900

    JIWON_Crawler Second Commit

--- project/Crawler/views.py
+++ project/Crawler/views.py
@@ -24,22 +24,21 @@ class CrawlingRouter(APIView):
 
     #데이터 분석
     def WordParsing(self,news_data):
-        okt = Okt() #twitter 형태소 분석기: 속도가 중요하고, 대량의 텍스트 데이터를 다루는 경우에 유용함.
-        
+        # okt = Okt() #twitter 형태소 분석기: 속도가 중요하고, 대량의 텍스트 데이터를 다루는 경우에 유용함.
+        hannanum = Hannanum()
         nouns_list =[]
         for i in range(len(news_data)):
             #무의미한 문자 삭제
             data = news_data[i]
-            
             replace_list = ["‘","’","”","“",'"',"'","[포토]","[사설]","[CarTalk]","...","…","[단독]"]
             for j in replace_list:
                 data = data.replace(j, "")
             
-
-            nouns = okt.nouns(data)
+            nouns = hannanum.nouns(data)
             nouns_list += nouns
-            
+            # print(nouns_list)
         nouns_list = [x for x in nouns_list if len(x)>1] #한 글자 삭제
+        
 
         counter = Counter(nouns_list)
         result = counter.most_common(10) #상위 10개 데이터
@@ -49,13 +48,13 @@ class CrawlingRouter(APIView):
             
     #크롤링(데이터 가져오기)
     def post(self, request):
-        #chrome_options = Options()
-        #chrome_options.add_argument('--headless')  # 백그라운드에서 실행
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')  # 백그라운드에서 실행
         #options=chrome_options
         
         #크롬 실행
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        with webdriver.Chrome(service=Service(ChromeDriverManager().install())) as driver:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
+        with webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options) as driver:
             
             #언론사 뉴스 홈-> 경향신문(첫번째)
             driver.get("https://news.naver.com/main/officeList.naver")
