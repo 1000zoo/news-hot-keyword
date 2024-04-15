7c0293d6c29763dad13c30951c6657d11fa4da69
lph <funglee0910@naver.com>
2024-04-15 20:20:51 +0900

    use admin to ckeck models & model few fix

--- project/app/models.py
+++ project/app/models.py
@@ -6,3 +6,6 @@ class Hotkeywords(models.Model):
     keyword_text = models.CharField(max_length=200)
     keyword_date = models.DateTimeField(auto_now_add=True)
     count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.keyword_text
