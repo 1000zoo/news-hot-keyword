98c8babad6894d98d2c08cad67cf8e1a45646584
1000zoo <cjswldn99@gmail.com>
2024-04-16 16:03:41 +0900

    feat: 오늘의 키워드 ui 추가

--- project/app/views.py
+++ project/app/views.py
@@ -6,6 +6,9 @@ from datetime import timedelta
 
 from .models import Hotkeywords
 
+def get_today_keywords():
+    return {"a":5, "b":2, "c":1, "d":10, "e":11, "f":12, "g": 1, "h": 4, "i":5, "j":6}
+
 def get_latest_keywords():
     return Hotkeywords.objects.all().order_by("-keyword_date")[:10]
 
@@ -18,7 +21,6 @@ def sorted_dict(dictionary: dict):
     return results
 
 def load_data():
-    response = requests.post("http://127.0.0.1:8000/api/crawling/")
     hotkeywords = get_latest_keywords()
     results = {}
     for hotkeyword in hotkeywords:
@@ -27,11 +29,16 @@ def load_data():
 
 # Create your views here.
 def index(request):
+    # response = requests.post("http://127.0.0.1:8000/api/crawling/")
     lastest_keywords = load_data()
-    context = {"hotkeywords": lastest_keywords}
+    today_keywords = get_today_keywords()
+    context = {"hotkeywords": lastest_keywords, "todaykeywords": today_keywords}
     return render(request, 'hotkeyword/index.html', context=context)
 
-def chart(request):
-    lastest_keywords = load_data()
-    context = {"hotkeywords": json.dumps(lastest_keywords)}
+def chart(request, id):
+    if id == 0:
+        keywords = load_data()
+    else:
+        keywords = get_today_keywords()
+    context = {"hotkeywords": json.dumps(keywords)}
     return render(request, 'hotkeyword/chart.html', context=context)
