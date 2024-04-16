4adf3cb44a034761ca7e304d809ca68664130b99
1000zoo <cjswldn99@gmail.com>
2024-04-16 13:29:21 +0900

    fix: 실시간 반영 안되는 오류 수정

--- project/app/views.py
+++ project/app/views.py
@@ -6,14 +6,24 @@ from datetime import timedelta
 
 from .models import Hotkeywords
 
+def get_latest_keywords():
+    return Hotkeywords.objects.all().order_by("-keyword_date")[:10]
+
+def sorted_dict(dictionary: dict):
+    results = {}
+    keys = sorted(dictionary.keys(), key=lambda k:dictionary[k])
+    keys.reverse()
+    for key in keys:
+        results[key] = dictionary[key]
+    return results
+
 def load_data():
-    # response = requests.post("http://127.0.0.1:8000/api/crawling/")
-    hotkeywords = Hotkeywords.objects.all()[:10]
+    response = requests.post("http://127.0.0.1:8000/api/crawling/")
+    hotkeywords = get_latest_keywords()
     results = {}
     for hotkeyword in hotkeywords:
         results[hotkeyword.keyword_text] = hotkeyword.count
-        print(hotkeyword.keyword_date)
-    return results
+    return sorted_dict(results)
 
 # Create your views here.
 def index(request):
